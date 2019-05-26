3eaf5ebd13a65fea23e10f0906d8094e75899804
小乌龟 <15810660676@163.com>
2019-05-26 09:30:25 +0800

    今天做了规格的审核，修改了数据库规格表，对于前端页面进行新增审核状态，更改到dao层good上进行SQL语句的更改(逆向工程的映射修改完毕)。然后更新pojo类增加审核项，在controller层中增加方法路径，brand接口中新增方法，进行实现类的完成。

--- pinyougou-parent/pinyougou-web-manager/src/main/webapp/js/service/typeTemplateService.js
+++ pinyougou-parent/pinyougou-web-manager/src/main/webapp/js/service/typeTemplateService.js
@@ -28,5 +28,11 @@ app.service('typeTemplateService',function($http){
 	//搜索
 	this.search=function(page,rows,searchEntity){
 		return $http.post('../typeTemplate/search.do?page='+page+"&rows="+rows, searchEntity);
-	}    	
+	}
+	//审核状态
+    this.updateStatus = function(ids,status){
+        return $http.get('../typeTemplate/updateStatus.do?ids='+ids+"&status="+status);
+    }
+
+
 });
